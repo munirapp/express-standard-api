ad93d726afe860134f675163c824bf81380279c8
munirapp <softwaremakassar@gmail.com>
2020-08-14 14:58:35 +0800

    update response

--- src/controllers/profileControllers.ts
+++ src/controllers/profileControllers.ts
@@ -9,9 +9,9 @@ const redisSetAsync = promisify(redisClient.setex).bind(redisClient);
 
 class ProfileControllers {
   async getAllProfile(req, res) {
+    // Initiate response data and response time
     let dataAllProfile = null;
     let time = response.start_time();
-
     try {
       // Get All Profile in Redis
       dataAllProfile = await redisGetAsync("allProfile");
@@ -45,19 +45,41 @@ class ProfileControllers {
   }
 
   async getPaginateProfile(req, res) {
+    // Initiate response data and response time
+    let dataPaginateProfile = null;
+    let time = response.start_time();
     try {
       // Initate Limit and Offset
       const { number } = req.params;
       const limit = 10;
       const offset = number ? limit * number : 0;
-      // Get Paginate Profile from Database
-      const dbPaginateProfile = await profile.paginate(limit, offset);
-      // Return Success response
+      // Get Paginate Profile in Redis
+      dataPaginateProfile = await redisGetAsync(`paginateProfile${number}`);
+      // Validate Cache
+      if (!dataPaginateProfile) {
+        // Get Paginate Profile from Database
+        dataPaginateProfile = await profile.paginate(limit, offset);
+        // Set data in Redis for 5 minutes
+        await redisSetAsync(
+          `paginateProfile${number}`,
+          300,
+          JSON.stringify(dataPaginateProfile)
+        );
+        // Return Success response from database
+        return res.json(
+          response.format(
+            time,
+            dataPaginateProfile,
+            "success load data from database"
+          )
+        );
+      }
+      // Return Success response from redis
       return res.json(
         response.format(
-          response.start_time(),
-          dbPaginateProfile,
-          "success load data from database"
+          time,
+          JSON.parse(dataPaginateProfile),
+          "success load data from cache"
         )
       );
     } catch (error) {
@@ -67,6 +89,9 @@ class ProfileControllers {
   }
 
   async getProfileID(req, res) {
+    // Initiate response data and response time
+    let dataProfileID = null;
+    let time = response.start_time();
     try {
       // Initate id
       const { id } = req.params;
@@ -77,18 +102,32 @@ class ProfileControllers {
           message: "ID can't be empty or must larger than zero",
         };
       }
-      // Get Detail Profile from Database
-      const dbProfileID = await profile.findByID(id);
-      // Validate result databse
-      if (dbProfileID.rows == 0) {
-        throw { code: 404, message: "Not found user" };
+      // Get Paginate Profile in Redis
+      dataProfileID = await redisGetAsync(`detailProfile${id}`);
+      if (!dataProfileID) {
+        // Get Detail Profile from Database
+        dataProfileID = await profile.findByID(id);
+        // Set data in Redis for 5 minutes
+        await redisSetAsync(
+          `detailProfile${id}`,
+          300,
+          JSON.stringify(dataProfileID)
+        );
+        // Return Success response from database
+        return res.json(
+          response.format(
+            time,
+            dataProfileID,
+            "success load data from database"
+          )
+        );
       }
-      // Return Success response
+      // Return Success response from cache
       return res.json(
         response.format(
-          response.start_time(),
-          dbProfileID,
-          "success load data from database"
+          time,
+          JSON.parse(dataProfileID),
+          "success load data from cache"
         )
       );
     } catch (error) {
