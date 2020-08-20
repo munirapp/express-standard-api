16c95563f3bbc4a1557d5db1a03dc540ccf00c3b
restuwahyu13 <restuwahyu13@gmail.com>
2020-08-20 11:25:01 +0700

    add oop pattern

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,3 @@
-import app from "./app";
-
-// Listen and running server
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Running express on port ${process.env.SERVER_PORT}`);
-});
+import { App } from './app'
+// init server
+new App().server()
