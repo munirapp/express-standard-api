30c06e65bd2eaea51bfb6008623e4fefa08a8211
restuwahyu13 <restuwahyu13@gmail.com>
2020-08-20 11:23:31 +0700

    fixed file

--- database/migrations/20200812193049_profile.ts
+++ database/migrations/20200812193049_profile.ts
@@ -1,15 +1,15 @@
-import * as Knex from "knex";
+import Knex from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable("profile", (table) => {
-    table.increments();
-    table.string("name", 50);
-    table.string("email", 50);
-    table.string("phone", 15);
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-  });
+  return knex.schema.createTable('profile', (table) => {
+    table.increments()
+    table.string('name', 50)
+    table.string('email', 50)
+    table.string('phone', 15)
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+  })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTableIfExists("profile");
+  return knex.schema.dropTableIfExists('profile')
 }
