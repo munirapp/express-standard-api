e15e36cc2e1c6a1231a6e491204ac5764dca37d1
restuwahyu13 <restuwahyu13@gmail.com>
2020-08-20 11:23:58 +0700

    fixed file

--- database/seeds/profiles.ts
+++ database/seeds/profiles.ts
@@ -1,19 +1,19 @@
-import * as Knex from "knex";
-import * as faker from "faker";
+import Knex from 'knex'
+import faker from 'faker'
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
-  await knex("profile").del();
+  await knex('profile').del()
 
   // Inserts seed entries
   for (let index = 1; index <= 10000; index++) {
-    await knex("profile").insert([
+    await knex('profile').insert([
       {
         id: index,
         name: faker.name.findName(),
         email: faker.internet.email(),
-        phone: faker.phone.phoneNumberFormat(),
-      },
-    ]);
+        phone: faker.phone.phoneNumberFormat()
+      }
+    ])
   }
 }
