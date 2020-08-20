48e09f91979c0609d730298c8339b34b2f9ffff5
restuwahyu13 <restuwahyu13@gmail.com>
2020-08-20 11:29:14 +0700

    add oop pattern

--- src/controllers/profileControllers.ts
+++ src/controllers/profileControllers.ts
@@ -1,8 +1,12 @@
-import profile from "../models/profile";
-import response from "../helper/response";
-import { redisGetAsync, redisSetAsync } from "../../redis";
+import { Model } from '../models/root.model'
+import response from '../helper/response'
+import { redisGetAsync, redisSetAsync } from '../../redis'
 
-class ProfileControllers {
+class ProfileControllers extends Model {
+  constructor() {
+    super()
+  }
+
   /**
    *
    * @param req express request object
@@ -27,37 +31,25 @@ class ProfileControllers {
    */
   async getAllProfile(req, res) {
     // Initiate response data and response time
-    let dataAllProfile = null;
-    let time = response.start_time();
+    let dataAllProfile = null
+    let time = response.start_time()
     try {
       // Get All Profile in Redis
-      dataAllProfile = await redisGetAsync("allProfile");
+      dataAllProfile = await redisGetAsync('allProfile')
       // Validate Cache
       if (!dataAllProfile) {
         // Get All Profile from Database
-        dataAllProfile = await profile.all();
+        dataAllProfile = await this.profileModel().all()
         // Set data in Redis for 5 minutes
-        await redisSetAsync("allProfile", 300, JSON.stringify(dataAllProfile));
+        await redisSetAsync('allProfile', 300, JSON.stringify(dataAllProfile))
         // Return Success response data from database
-        return res.json(
-          response.format(
-            time,
-            dataAllProfile,
-            "success load data from database"
-          )
-        );
+        return res.json(response.format(time, dataAllProfile, 'success load data from database'))
       }
       // Return Success response data from redis
-      return res.json(
-        response.format(
-          time,
-          JSON.parse(dataAllProfile),
-          "success load data from cache"
-        )
-      );
+      return res.json(response.format(time, JSON.parse(dataAllProfile), 'success load data from cache'))
     } catch (error) {
       // Return Error response
-      return res.status(500).send(error);
+      return res.status(500).send(error)
     }
   }
 
@@ -90,45 +82,29 @@ class ProfileControllers {
    */
   async getPaginateProfile(req, res) {
     // Initiate response data and response time
-    let dataPaginateProfile = null;
-    let time = response.start_time();
+    let dataPaginateProfile = null
+    let time = response.start_time()
     try {
       // Initate Limit and Offset
-      const { number } = req.params;
-      const limit = 10;
-      const offset = number ? limit * number : 0;
+      const { number } = req.params
+      const limit = 10
+      const offset = number ? limit * number : 0
       // Get Paginate Profile in Redis
-      dataPaginateProfile = await redisGetAsync(`paginateProfile${number}`);
+      dataPaginateProfile = await redisGetAsync(`paginateProfile${number}`)
       // Validate Cache
       if (!dataPaginateProfile) {
         // Get Paginate Profile from Database
-        dataPaginateProfile = await profile.paginate(limit, offset);
+        dataPaginateProfile = await this.profileModel().paginate(limit, offset)
         // Set data in Redis for 5 minutes
-        await redisSetAsync(
-          `paginateProfile${number}`,
-          300,
-          JSON.stringify(dataPaginateProfile)
-        );
+        await redisSetAsync(`paginateProfile${number}`, 300, JSON.stringify(dataPaginateProfile))
         // Return Success response from database
-        return res.json(
-          response.format(
-            time,
-            dataPaginateProfile,
-            "success load data from database"
-          )
-        );
+        return res.json(response.format(time, dataPaginateProfile, 'success load data from database'))
       }
       // Return Success response from redis
-      return res.json(
-        response.format(
-          time,
-          JSON.parse(dataPaginateProfile),
-          "success load data from cache"
-        )
-      );
+      return res.json(response.format(time, JSON.parse(dataPaginateProfile), 'success load data from cache'))
     } catch (error) {
       // Return Error response
-      return res.status(500).send(error);
+      return res.status(500).send(error)
     }
   }
 
@@ -163,54 +139,38 @@ class ProfileControllers {
    */
   async getProfileID(req, res) {
     // Initiate response data and response time
-    let dataProfileID = null;
-    let time = response.start_time();
+    let dataProfileID = null
+    let time = response.start_time()
     try {
       // Initate id
-      const { id } = req.params;
+      const { id } = req.params
       // Validate id
       if (!id || id == 0) {
         throw {
           code: 400,
-          message: "ID can't be empty or must larger than zero",
-        };
+          message: "ID can't be empty or must larger than zero"
+        }
       }
       // Get Paginate Profile in Redis
-      dataProfileID = await redisGetAsync(`detailProfile${id}`);
+      dataProfileID = await redisGetAsync(`detailProfile${id}`)
       if (!dataProfileID) {
         // Get Detail Profile from Database
-        dataProfileID = await profile.findByID(id);
+        dataProfileID = await this.profileModel().findByID(id)
         // Set data in Redis for 5 minutes
-        await redisSetAsync(
-          `detailProfile${id}`,
-          300,
-          JSON.stringify(dataProfileID)
-        );
+        await redisSetAsync(`detailProfile${id}`, 300, JSON.stringify(dataProfileID))
         // Return Success response from database
-        return res.json(
-          response.format(
-            time,
-            dataProfileID,
-            "success load data from database"
-          )
-        );
+        return res.json(response.format(time, dataProfileID, 'success load data from database'))
       }
       // Return Success response from cache
-      return res.json(
-        response.format(
-          time,
-          JSON.parse(dataProfileID),
-          "success load data from cache"
-        )
-      );
+      return res.json(response.format(time, JSON.parse(dataProfileID), 'success load data from cache'))
     } catch (error) {
       // Validate and Return Error response
-      if (typeof error == "object") {
-        return res.status(error.code).send(error.message);
+      if (typeof error == 'object') {
+        return res.status(error.code).send(error.message)
       }
-      return res.status(500).send(error);
+      return res.status(500).send(error)
     }
   }
 }
 
-export default new ProfileControllers();
+export default new ProfileControllers()
