0fd3549abc5ebd580c9c72beedb547dad6bd0d8d
restuwahyu13 <restuwahyu13@gmail.com>
2020-08-20 11:22:48 +0700

    fixed file

--- redis.ts
+++ redis.ts
@@ -1,18 +1,18 @@
-import * as redis from "redis";
-import * as dotenv from "dotenv";
-import { promisify } from "util";
+import redis from 'redis'
+import dotenv from 'dotenv'
+import { promisify } from 'util'
 
 // Initiate dotenv config
-dotenv.config();
+dotenv.config()
 // Create redis client connection
-const client = redis.createClient({ host: process.env.REDIS_HOST });
+const client = redis.createClient({ host: process.env.REDIS_HOST })
 // Wrapper redis get and set in Promise
-const GetAsync = promisify(client.get).bind(client);
-const SetAsync = promisify(client.setex).bind(client);
-const DelAsync = promisify(client.del).bind(client);
-const FlushAsync = promisify(client.flushall).bind(client);
+const GetAsync = promisify(client.get).bind(client)
+const SetAsync = promisify(client.setex).bind(client)
+const DelAsync = promisify(client.del).bind(client)
+const FlushAsync = promisify(client.flushall).bind(client)
 
-export const redisGetAsync = GetAsync;
-export const redisSetAsync = SetAsync;
-export const redisDelAsync = DelAsync;
-export const redisFlushAsync = FlushAsync;
+export const redisGetAsync = GetAsync
+export const redisSetAsync = SetAsync
+export const redisDelAsync = DelAsync
+export const redisFlushAsync = FlushAsync
